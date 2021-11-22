4803b8f9840089a7a5224373a674ddf441614650
danyzaher <dany0.zaher@etu.u-pec.fr>
2021-11-22 15:46:06 +0100

    icon and manual config

--- src/main/java/episen/pds/citizens/frontend/controllers/UseMonitorController.java
+++ src/main/java/episen/pds/citizens/frontend/controllers/UseMonitorController.java
@@ -1,7 +1,6 @@
 package episen.pds.citizens.frontend.controllers;
 
 import episen.pds.citizens.frontend.model.RoomsWithConsumption;
-import episen.pds.citizens.frontend.model.Test;
 import episen.pds.citizens.frontend.service.UseMonitorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,7 +23,7 @@ public class UseMonitorController {
     @GetMapping("/listRooms")
     public String getRooms(Model model){
         Iterable<RoomsWithConsumption> listRoom = useMonitorService.getRooms();
-        model.addAttribute("room", listRoom);
-        return "configmanu";
+        model.addAttribute("rooms", listRoom);
+        return "showmyrooms";
     }
 }

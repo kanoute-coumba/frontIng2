94881ee691e181150d843113daa140999dc473ad
prisca.clio <prisca.clio@gmail.com>
2021-12-09 12:15:21 +0100

    addition of a column and automatic refresh of the page currentMix

--- src/main/java/episen/pds/citizens/frontend/model/MixEnBySite.java
+++ src/main/java/episen/pds/citizens/frontend/model/MixEnBySite.java
@@ -3,12 +3,14 @@ package episen.pds.citizens.frontend.model;
 public class MixEnBySite {
     private int mix;
     private String address;
+    private String name_building;
 
     public MixEnBySite(){}
 
-    public MixEnBySite(int mix, String name_building){
+    public MixEnBySite(int mix, String address, String name_building){
         this.mix=mix;
-        this.address=name_building;
+        this.address=address;
+        this.name_building=name_building;
     }
 
     public int getMix(){
@@ -20,8 +22,14 @@ public class MixEnBySite {
     public String getAddress(){
         return address;
     }
-    public void setAddress(String name_building){
-        this.address=name_building;
+    public void setAddress(String address){
+        this.address=address;
+    }
+    public String getName_building(){
+        return name_building;
+    }
+    public void setName_building(String name_building){
+        this.name_building=name_building;
     }
     public String toString(){
         return ""+address+" "+mix;

0d0bec84f2d600aa1f00830a94701ddae46e2fde
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-12-13 13:04:05 +0100

    new version cafeteria

--- src/main/java/episen/pds/citizens/frontend/controllers/MenuController.java
+++ src/main/java/episen/pds/citizens/frontend/controllers/MenuController.java
@@ -2,25 +2,20 @@ package episen.pds.citizens.frontend.controllers;
 
 
 import episen.pds.citizens.frontend.model.Menu;
-import episen.pds.citizens.frontend.model.Test;
+import episen.pds.citizens.frontend.model.Menu_reservation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import episen.pds.citizens.frontend.service.MenuService;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger;
 
 
 @Controller
 public class MenuController {
 
     MenuService menuService = new MenuService();
+    private static final Logger logger = Logger.getLogger(MenuController.class.getName());
 
 
     @GetMapping("/cafeteria")
@@ -34,18 +29,36 @@ public class MenuController {
         return "cafeteria";
 
     }
+    @GetMapping("/cafeteria_reservation/{num_menu}")
+    public String reserveMenu(@PathVariable("num_menu") String num_menu, Model model) {
+        System.out.println(num_menu);
+        Menu_reservation menu_reservation = new Menu_reservation();
+        model.addAttribute("menu_reservation", menu_reservation);
+        model.addAttribute("num_menu", num_menu);
+        return "cafeteria_reservation";
+    }
 
-   @GetMapping("/cafeteria_reservation/saved")
-    public String savedMenu(Model model, @ModelAttribute Menu menu) {
+
+
+    @PostMapping("/cafeteria_reservation/saved")
+    public String formReservation(Model model, @ModelAttribute Menu_reservation menu_reservation) {
         //ModelAttribut récupère l'objet crée
-        //model.addAttribute("reserved-menu", menuService.getMenuById(1));
-        //menuService.saveMenu(menu);
+        model.addAttribute("menu_reservation", menu_reservation);
         return "cafeteria_reservation_saved";
     }
 
-    @GetMapping("/cafeteria_reservation")
-    public String reserveMenu(Model model, @ModelAttribute Menu menu) {
-        return "cafeteria_reservation";
+    @PostMapping("/cafeteria_reservation/saved/confirm")
+    public String saveReservation (Model model, @ModelAttribute("menu_reservation") Menu_reservation menu_reservation) {
+        logger.info(menu_reservation.toString());
+        menuService.saveMenu(menu_reservation);
+        return "redirect:/cafeteria";
     }
+
+    /*@GetMapping("/cafeteria_reservation/saved")
+    public String reserveMenu(Model model, @ModelAttribute Menu_reservation menu_reservation) {
+        Menu_reservation Menu_reservation = menuService.getMenuReservation(1);
+        model.addAttribute("menu_reservation", menu_reservation);
+        return "cafeteria_reservation_saved";
+    }*/
 
 }

4fffea2f5bab87fd01480f4d1b1c8ffdeaaa3339
kanoute-coumba <kanoute.coumba@gmail.com>
2021-12-13 12:33:57 +0100

    overrun fornt side

---
+++ src/main/java/episen/pds/citizens/frontend/service/OverrunService.java
@@ -0,0 +1,11 @@
+package episen.pds.citizens.frontend.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class OverrunService {
+
+    private static final Logger logger = Logger.getLogger(OverrunService.class.getName());
+}

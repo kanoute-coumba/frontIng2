48c09aebafb777b9d1bb0d52577f49c0ea5e7b22
prisca.clio <prisca.clio@gmail.com>
2022-02-27 16:29:52 +0100

    Algorithm selection page

--- src/main/java/episen/pds/citizens/frontend/controllers/MixEnController.java
+++ src/main/java/episen/pds/citizens/frontend/controllers/MixEnController.java
@@ -1,12 +1,14 @@
 package episen.pds.citizens.frontend.controllers;
 
+import episen.pds.citizens.frontend.model.ChoixAlgoForm;
 import episen.pds.citizens.frontend.model.MixEn;
 import episen.pds.citizens.frontend.model.MixEnBySite;
-import episen.pds.citizens.frontend.repository.MixEnProxy;
 import episen.pds.citizens.frontend.service.MixEnService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.logging.Logger;
 
@@ -16,7 +18,7 @@ public class MixEnController {
 
     private MixEnService mixEnService = new MixEnService();
 
-    // service Current Mix
+    // controller Current Mix
 
     @GetMapping("/Mix")
     public String getListTest(Model model) {
@@ -43,11 +45,21 @@ public class MixEnController {
         return "currentMix";
     }
 
-    // Service Choice Algo
+    // controller Choice Algo for the Mix
 
     @GetMapping("/AlgoMix")
-    public String getChoiceAlgo(){
+    public String getChoiceAlgo(Model model){
+        model.addAttribute("choix",new ChoixAlgoForm());
 
         return "choiceAlgo";
+    }
+
+    @PostMapping("/AlgoMix")
+    public String getChoiceAlgoSubmit(@ModelAttribute ChoixAlgoForm model){
+        //String t= (String) model.getAttribute("choix_algo");
+        logger.info(""+model.getChoice());
+        logger.info(""+model.toString());
+
+        return "redirect:/Municipality"; //TODO
     }
 }
--- src/main/java/episen/pds/citizens/frontend/controllers/MunicipalityPageController.java
+++ src/main/java/episen/pds/citizens/frontend/controllers/MunicipalityPageController.java
@@ -2,6 +2,7 @@ package episen.pds.citizens.frontend.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.logging.Logger;
 

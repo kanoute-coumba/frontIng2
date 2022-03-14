8b1f0b0578546595a9127477fc8837d807ef593e
yorgunmichael <yorgunchacha@yahoo.fr>
2022-03-14 22:13:50 +0100

    first merger front

--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -1,14 +1,14 @@
-function openChat() {
-    document.getElementById('chatbox').style.display = 'block';
-    $('.btn-chat').hide();
-}
-
-function closeChat() {
-    document.getElementById('chatbox').style.display = 'none';
-    $('.btn-chat').show();
-}
-
-$('form').submit(function () {
-    var text = $(this).find("textarea[name='msg']").val();
-    alert(text)
-}
+// function openChat() {
+//     document.getElementById('chatbox').style.display = 'block';
+//     $('.btn-chat').hide();
+// }
+//
+// function closeChat() {
+//     document.getElementById('chatbox').style.display = 'none';
+//     $('.btn-chat').show();
+// }
+//
+// $('form').submit(function () {
+//     var text = $(this).find("textarea[name='msg']").val();
+//     alert(text)
+// }

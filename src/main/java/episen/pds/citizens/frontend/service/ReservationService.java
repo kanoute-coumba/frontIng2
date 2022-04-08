5e3f89c9185612724244e708c556f77551370953
Amal Mimouni <mimouni.amal2000@gmail.com>
2022-04-08 02:16:59 +0200

    [ADD] : start reservation

---
+++ src/main/java/episen/pds/citizens/frontend/service/ReservationService.java
@@ -0,0 +1,48 @@
+package episen.pds.citizens.frontend.service;
+
+import episen.pds.citizens.frontend.model.Reservation;
+import episen.pds.citizens.frontend.model.architectureModel.Space;
+import episen.pds.citizens.frontend.repository.MessagesProxy;
+import episen.pds.citizens.frontend.repository.ReservationProxy;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+public class ReservationService {
+
+    @Autowired
+    private ReservationProxy reservationProxy = new ReservationProxy();
+    public Reservation getReservationById(int id) {
+        //System.out.print(MessagesProxy.getMessagesById(id).getClass());
+        return reservationProxy.getReservationById(id);
+    }
+
+    public Iterable<Reservation> getReservation() {
+        //System.out.print(MessagesProxy.getMessagesById(id).getClass());
+        return reservationProxy.getReservation();
+    }
+
+    public Iterable<Space> getReservesSpace() {
+        //System.out.print(MessagesProxy.getMessagesById(id).getClass());
+        return reservationProxy.getReservedSpace();
+    }
+
+
+
+    /*public Messages_reservation saveMessages(Messages_reservation messages_reservation) {
+        return messagesProxy.reserveMessages(messages_reservation);
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
+
